13c42f837a07fe2d24312d3e470f8bdf8f91cdb7
Lobao <web@emd.art.br>
2022-02-16 15:50:17 -0300

    using resolvers for relacionation

--- schemaql/projeto/index.js
+++ schemaql/projeto/index.js
@@ -1,154 +1,177 @@
-const { ApolloServer, gql } = require('apollo-server')
+const { ApolloServer, gql } = require("apollo-server");
 
+// Criando dados
 
 const usuarios = [
-    {
-        id: 1,
-        nome: "Priscila Amor",
-        email: "pri@mail.com",
-        idade: 31,
-        salario: 1550.00
-    }, {
-        id: 2,
-        nome: "Jonas Lobo",
-        email: "lobo@mail.com",
-        idade: 37,
-    }, {
-        id: 4,
-        nome: "Pietro Lobo",
-        email: "pietro@mail.com",
-        idade: 31,
-    }, {
-        id: 4,
-        nome: "Maria Sogra",
-        email: "maria@mail.com",
-        idade: 60,
-    },    
-    ]
+  {
+    id: 1,
+    nome: "Priscila Amor",
+    email: "pri@mail.com",
+    idade: 31,
+    salario: 1550.0,
+    perfil_id: 1, 
+  },
+  {
+    id: 2,
+    nome: "Jonas Lobo",
+    email: "lobo@mail.com",
+    idade: 37,
+    perfil_id: 2,
+  },
+  {
+    id: 4,
+    nome: "Pietro Lobo",
+    email: "pietro@mail.com",
+    idade: 31,
+    perfil_id: 3, 
+  },
+  {
+    id: 4,
+    nome: "Maria Sogra",
+    email: "maria@mail.com",
+    idade: 60,
+    perfil_id: 3,
+  },
+];
 
-const perfis = [
-    { id: 1, nome: "comum" },
-    { id:2, nome: "adminstrador"},
-]
+const Perfil = [
+  { id: 1, nome: "comum" },
+  { id: 2, nome: "adminstrador" },
+  { id: 3, nome: "gerente" }, 
+];
+
+// Criando tipos de dados e solicitando através das querys
 
 const typeDefs = gql`
-     scalar Date
+  scalar Date
 
-    type Produto {
-        nome: String!
-        preco: Float!
-        desconto: Float
-        precoComDesconto: Float 
-    }
+  type Produto {
+    nome: String!
+    preco: Float!
+    desconto: Float
+    precoComDesconto: Float
+  }
 
-    type perfis {
-        id: Int 
-        nome: String 
-    }  
+  type Usuario {
+    id: ID
+    nome: String!
+    email: String!
+    idade: Int
+    salario: Float
+    vip: Boolean
+    perfil: Perfil     
+  }
 
-    type Usuario {
-        id: ID
-        nome: String!
-        email: String!
-        idade: Int
-        salario: Float
-        vip: Boolean 
-    }
+  type Perfil {
+    id: Int 
+    nome: String
+  }
 
-    type Perfil {
-        id: ID
-        nome: String
-    }
+  # Pontos de entrada da sua API!
+  type Query {
+    ola: String
+    horaAtual: Date
+    saudacao: String
+    usuarioLogado: Usuario
+    produtoEmDestaque: Produto
+    numerosMegaSena: [Int!]!
+    usuarios: [Usuario]
+    usuario(id: ID): Usuario
+    perfis: [Perfil]
+    perfil(id: Int): Perfil   
+  }
+`;
 
-    # Pontos de entrada da sua API!
-    type Query {
-        ola: String  
-        horaAtual: Date    
-        saudacao: String 
-        usuarioLogado: Usuario
-        produtoEmDestaque: Produto
-        numerosMegaSena: [Int!]! 
-        usuarios: [Usuario]
-        usuario(id: ID): Usuario
-        perfis: [Perfil]
-        perfil(id: Int): Perfil        
-        
-    }
-`
+// Resolvendo os dados solicitados
+
 const resolvers = {
-    // Scalar o tipo da Query 
-    Produto: {
-        precoComDesconto(produto) {
-            if (produto.desconto) {
-                return produto.preco * (1 - produto.desconto)
-            } else {
-                return produto.preco
-            }
-        }
+  // Scalar o tipo da Query
+  Produto: {
+    precoComDesconto(produto) {
+      if (produto.desconto) {
+        return produto.preco * (1 - produto.desconto);
+      } else {
+        return produto.preco;
+      }
+    },
+  },
+
+  Usuario: {
+    salario(usuario) {
+      return usuario.salario_real 
     },
 
-    //Criando resolvers dentroda Query
+    perfil(usuario) {
+      const profile = Perfil.filter((p) => p.id === usuario.perfil_id);
+      return profile ? profile[0] : null;
+    }, 
+  },
 
-    Query: {
-        ola() {
-            return 'Show de bola'
-        },
-        horaAtual() {
-            return new Date
-        },
-        saudacao() {
-            return `Fala brother, como vai?`
-        },
-        usuarioLogado() {
-            return {
-                id: 1,
-                nome: 'Jonas Lobo',
-                email: 'jonaslobo@mail.com',
-                idade: 37,
-                salario: 3000,
-                vip: true
-            }
-        },
-        produtoEmDestaque() {
-            return {
-                nome: 'Notbook',
-                preco: 15.000,
-                desconto: 0.10
-            }
-        },
-        numerosMegaSena() {
-            // return [4, 8, 12, 15, 62, 77]
-            function crescente(a, b) {
-                return a - b
-            }
-            return Array(6).fill(0)
-                .map(n => parseInt(Math.random() * 60 + 1))
-                .sort(crescente)
-        },
-        usuarios() {
-            return usuarios
-        },
-        usuario(_, { id }){
-            const selec = usuarios.filter(u => u.id == id)
-            return selec ? selec[0] : null 
-            
-        },
-        perfis() {
-            return perfis 
-        },
-        perfil(_, { id }) {
-            const seletor = perfis.filter(p => p.id === id)
-            return seletor ? seletor[0] : null 
-        }       
-    }
-}
+
+
+  //Criando resolvers dentro da Query
+
+  Query: {
+    ola() {
+      return "Show de bola";
+    },
+    horaAtual() {
+      return new Date();
+    },
+    saudacao() {
+      return `Fala brother, como vai?`;
+    },
+    usuarioLogado() {
+      return {
+        id: 1,
+        nome: "Jonas Lobo",
+        email: "jonaslobo@mail.com",
+        idade: 37,
+        salario: 3000,
+        vip: true,
+      };
+    },
+    produtoEmDestaque() {
+      return {
+        nome: "Notbook",
+        preco: 15.0,
+        desconto: 0.1,
+      };
+    },
+    numerosMegaSena() {
+      // return [4, 8, 12, 15, 62, 77]
+      function crescente(a, b) {
+        return a - b;
+      }
+      return Array(6)
+        .fill(0)
+        .map((n) => parseInt(Math.random() * 60 + 1))
+        .sort(crescente);
+    },
+    usuarios() {
+      return usuarios;
+    },
+    usuario(_, { id }) {
+      const selec = usuarios.filter((u) => u.id == id);
+      return selec ? selec[0] : null;
+    },
+    perfis() {
+      return Perfil;
+    },
+    perfil(_, { id }) {
+      const profile = Perfil.filter((p) => p.id == id);
+      return profile ? profile[0] : null;
+    },
+  },
+};
+
+// Servidor
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers
-
-})
+  typeDefs,
+  resolvers,
+});
 
 server.listen().then(({ url }) => {
-    console.log(`Executando em ${url}`)
-})
+  console.log(`Executando em ${url}`);
+});
