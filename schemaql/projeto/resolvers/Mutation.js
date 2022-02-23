05640fc988f6960d88e35738630a8e851e72de06
Jonas <web@emd.art.br>
2022-02-23 14:56:43 -0300

    delete user with mutation

--- schemaql/projeto/resolvers/Mutation.js
+++ schemaql/projeto/resolvers/Mutation.js
@@ -1,18 +1,31 @@
 const { usuarios, nextid } = require('../data/db')
 
 module.exports = {
-    novoUsuario(_, { nome, email, idade }) {
+    novoUsuario(_,  args) {
+        const emailExistente = usuarios 
+        .some(u => u.email === args.email)
+
+        if (emailExistente) {
+            throw new Error('Ops! Este email já foi cadastrado, tente outra vez!')
+        }
+
         const novo = {
             id: nextid(),
-            nome,
-            email,
-            idade,
+            ...args,            
             perfil_id: 1,
             status: 'ATIVO'
         }
         
         usuarios.push(novo)
         return novo
+    }, 
+    excluirUsuario(_, { id }) {
+        const i = usuarios.findIndex(u => u.id === id)
+        if(i , 0) return null 
+        const excluidos = usuarios.splice(i, 1)
+        return excluidos ? excluidos[0] : null 
     }
+
+    
 }
 
